62b424624bfcba0f719d577ea23c5088ffabca57
Craig MacFarlane <camacfarl@gmail.com>
2017-09-02 15:35:59 -0700

    Fix breakages due to SDK public api changes.

--- java/team25core/AMSColorSensorImproved.java
+++ java/team25core/AMSColorSensorImproved.java
@@ -42,6 +42,8 @@ import com.qualcomm.robotcore.hardware.I2cDeviceSynch;
 import com.qualcomm.robotcore.hardware.I2cDeviceSynchDevice;
 import com.qualcomm.robotcore.hardware.I2cDeviceSynchImpl;
 import com.qualcomm.robotcore.hardware.I2cDeviceSynchSimple;
+import com.qualcomm.robotcore.hardware.I2cWaitControl;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 import com.qualcomm.robotcore.util.RobotLog;
 import com.qualcomm.robotcore.util.TypeConversion;
 
@@ -62,9 +64,81 @@ import java.nio.ByteOrder;
  * Importantly, this implementation sits on top of I2cDeviceSyncSimple instead of I2cDevice
  */
 public class AMSColorSensorImproved extends I2cDeviceSynchDevice<I2cDeviceSynchSimple> implements AMSColorSensor {
+    @Override
+    public NormalizedRGBA getNormalizedColors()
+    {
+        return null;
+    }
     //----------------------------------------------------------------------------------------------
     // State
     //----------------------------------------------------------------------------------------------
+
+    enum IntegrationTime
+    {
+        MS_2_4(0xFF),
+        MS_24(0xF6),
+        MS_50(0xEB),
+        MS_101(0xD5),
+        MS_154(0xC0),
+        MS_700(0x00);
+
+        public final byte byteVal;
+        IntegrationTime(int i) { this.byteVal = (byte) i; }
+    }
+
+    int AMS_COLOR_ENABLE = 0x00;
+    int AMS_COLOR_ENABLE_PIEN = 0x20;        /* Proximity interrupt enable */
+    int AMS_COLOR_ENABLE_AIEN = 0x10;        /* RGBC Interrupt Enable */
+    int AMS_COLOR_ENABLE_WEN = 0x08;         /* Wait enable - Writing 1 activates the wait timer */
+    int AMS_COLOR_ENABLE_PEN = 0x04;         /* Proximity enable */
+    int AMS_COLOR_ENABLE_AEN = 0x02;         /* RGBC Enable - Writing 1 actives the ADC, 0 disables it */
+    int AMS_COLOR_ENABLE_PON = 0x01;         /* Power on - Writing 1 activates the internal oscillator, 0 disables it */
+    int AMS_COLOR_ATIME = 0x01;              /* Integration time */
+    int AMS_COLOR_WTIME = 0x03;              /* Wait time = if AMS_COLOR_ENABLE_WEN is asserted; */
+    int AMS_COLOR_WTIME_2_4MS = 0xFF;        /* WLONG0 = 2.4ms   WLONG1 = 0.029s */
+    int AMS_COLOR_WTIME_204MS = 0xAB;        /* WLONG0 = 204ms   WLONG1 = 2.45s  */
+    int AMS_COLOR_WTIME_614MS = 0x00;        /* WLONG0 = 614ms   WLONG1 = 7.4s   */
+    int AMS_COLOR_AILTL = 0x04;              /* Clear channel lower interrupt threshold */
+    int AMS_COLOR_AILTH = 0x05;
+    int AMS_COLOR_AIHTL = 0x06;              /* Clear channel upper interrupt threshold */
+    int AMS_COLOR_AIHTH = 0x07;
+    int AMS_COLOR_PERS = 0x0C;               /* Persistence register - basic SW filtering mechanism for interrupts */
+    int AMS_COLOR_PERS_NONE = 0b0000;        /* Every RGBC cycle generates an interrupt                                */
+    int AMS_COLOR_PERS_1_CYCLE = 0b0001;     /* 1 clean channel value outside threshold range generates an interrupt   */
+    int AMS_COLOR_PERS_2_CYCLE = 0b0010;     /* 2 clean channel values outside threshold range generates an interrupt  */
+    int AMS_COLOR_PERS_3_CYCLE = 0b0011;     /* 3 clean channel values outside threshold range generates an interrupt  */
+    int AMS_COLOR_PERS_5_CYCLE = 0b0100;     /* 5 clean channel values outside threshold range generates an interrupt  */
+    int AMS_COLOR_PERS_10_CYCLE = 0b0101;    /* 10 clean channel values outside threshold range generates an interrupt */
+    int AMS_COLOR_PERS_15_CYCLE = 0b0110;    /* 15 clean channel values outside threshold range generates an interrupt */
+    int AMS_COLOR_PERS_20_CYCLE = 0b0111;    /* 20 clean channel values outside threshold range generates an interrupt */
+    int AMS_COLOR_PERS_25_CYCLE = 0b1000;    /* 25 clean channel values outside threshold range generates an interrupt */
+    int AMS_COLOR_PERS_30_CYCLE = 0b1001;    /* 30 clean channel values outside threshold range generates an interrupt */
+    int AMS_COLOR_PERS_35_CYCLE = 0b1010;    /* 35 clean channel values outside threshold range generates an interrupt */
+    int AMS_COLOR_PERS_40_CYCLE = 0b1011;    /* 40 clean channel values outside threshold range generates an interrupt */
+    int AMS_COLOR_PERS_45_CYCLE = 0b1100;    /* 45 clean channel values outside threshold range generates an interrupt */
+    int AMS_COLOR_PERS_50_CYCLE = 0b1101;    /* 50 clean channel values outside threshold range generates an interrupt */
+    int AMS_COLOR_PERS_55_CYCLE = 0b1110;    /* 55 clean channel values outside threshold range generates an interrupt */
+    int AMS_COLOR_PERS_60_CYCLE = 0b1111;    /* 60 clean channel values outside threshold range generates an interrupt */
+    int AMS_COLOR_CONFIG = 0x0D;
+    int AMS_COLOR_CONFIG_NORMAL = 0x00;      /* normal wait times */
+    int AMS_COLOR_CONFIG_WLONG = 0x02;       /* Extended wait time = 12x normal wait times via AMS_COLOR_WTIME */
+    int AMS_COLOR_CONTROL = 0x0F;            /* Set the gain level for the sensor */
+    int AMS_COLOR_GAIN_1 = 0x00;             /* normal gain */
+    int AMS_COLOR_GAIN_4 = 0x01;             /* 4x gain */
+    int AMS_COLOR_GAIN_16 = 0x02;            /* 16x gain */
+    int AMS_COLOR_GAIN_60 = 0x03;            /* 60x gain */
+    int AMS_COLOR_ID = 0x12;                 /* 0x44 = TCS34721/AMS_COLOR, 0x4D = TCS34723/TCS34727 */
+    int AMS_COLOR_STATUS = 0x13;
+    int AMS_COLOR_STATUS_AINT = 0x10;        /* RGBC Clean channel interrupt */
+    int AMS_COLOR_STATUS_AVALID = 0x01;      /* Indicates that the RGBC channels have completed an integration cycle */
+    int AMS_COLOR_CDATAL = 0x14;             /* Clear channel data */
+    int AMS_COLOR_CDATAH = 0x15;
+    int AMS_COLOR_RDATAL = 0x16;             /* Red channel data */
+    int AMS_COLOR_RDATAH = 0x17;
+    int AMS_COLOR_GDATAL = 0x18;             /* Green channel data */
+    int AMS_COLOR_GDATAH = 0x19;
+    int AMS_COLOR_BDATAL = 0x1A;             /* Blue channel data */
+    int AMS_COLOR_BDATAH = 0x1B;
 
     protected AMSColorSensor.Parameters parameters;
 
@@ -76,10 +150,9 @@ public class AMSColorSensorImproved extends I2cDeviceSynchDevice<I2cDeviceSynchS
     {
         super(deviceClient, isOwned);
         this.parameters = params;
-        this.parameters.integrationTime = IntegrationTime.MS_700;
         this.deviceClient.setLogging(this.parameters.loggingEnabled);
         this.deviceClient.setLoggingTag(this.parameters.loggingTag);
-        super.registerArmingStateCallback();
+        super.registerArmingStateCallback(false);
         this.engage();
     }
 
@@ -189,7 +262,7 @@ public class AMSColorSensorImproved extends I2cDeviceSynchDevice<I2cDeviceSynchS
     private void setGain(Gain gain)
     {
         // 3782 must set bit 5 as 1. On 3472, that must be written as zero. Ugh.
-        write8(Register.CONTROL, gain.byteVal | (is3782() ? 0x20 : 0));
+        write8(Register.CONTROL, gain.bVal | (is3782() ? 0x20 : 0));
     }
 
     public void reportEnable()
@@ -236,7 +309,7 @@ public class AMSColorSensorImproved extends I2cDeviceSynchDevice<I2cDeviceSynchS
     @Override
     public synchronized int alpha()
     {
-        return this.readColorRegister(Register.CLEAR);
+        return this.readColorRegister(Register.ALPHA);
     }
 
     private int readColorRegister(Register reg)
@@ -247,7 +320,7 @@ public class AMSColorSensorImproved extends I2cDeviceSynchDevice<I2cDeviceSynchS
     @Override
     public synchronized int argb()
     {
-        byte[] bytes = read(Register.CLEAR, 8);
+        byte[] bytes = read(Register.ALPHA, 8);
         ByteBuffer buffer = ByteBuffer.wrap(bytes).order(ByteOrder.LITTLE_ENDIAN);
         //
         int clear = TypeConversion.unsignedShortToInt(buffer.getShort());
@@ -300,30 +373,29 @@ public class AMSColorSensorImproved extends I2cDeviceSynchDevice<I2cDeviceSynchS
     @Override
     public synchronized byte read8(final Register reg)
     {
-        return deviceClient.read8(reg.byteVal | AMS_COLOR_COMMAND_BIT);
+        return deviceClient.read8(reg.bVal | AMS_COLOR_COMMAND_BIT);
     }
 
     @Override
     public synchronized byte[] read(final Register reg, final int cb)
     {
-        return deviceClient.read(reg.byteVal | AMS_COLOR_COMMAND_BIT, cb);
+        return deviceClient.read(reg.bVal | AMS_COLOR_COMMAND_BIT, cb);
     }
 
     @Override
     public synchronized void write8(Register reg, int data)
     {
-        this.deviceClient.write8(reg.byteVal | AMS_COLOR_COMMAND_BIT, data);
-        this.deviceClient.waitForWriteCompletions();
+        this.deviceClient.write8(reg.bVal | AMS_COLOR_COMMAND_BIT, data);
+        this.deviceClient.waitForWriteCompletions(I2cWaitControl.ATOMIC);
     }
 
     @Override
     public synchronized void write(Register reg, byte[] data)
     {
-        this.deviceClient.write(reg.byteVal | AMS_COLOR_COMMAND_BIT, data);
-        this.deviceClient.waitForWriteCompletions();
+        this.deviceClient.write(reg.bVal | AMS_COLOR_COMMAND_BIT, data);
+        this.deviceClient.waitForWriteCompletions(I2cWaitControl.ATOMIC);
     }
 
-    @Override
     public int readUnsignedShort(Register reg)
     {
         byte[] bytes = this.read(reg, 2);
@@ -335,7 +407,6 @@ public class AMSColorSensorImproved extends I2cDeviceSynchDevice<I2cDeviceSynchS
         return result;
     }
 
-    @Override
     public void writeShort(Register ireg, int value)
     {
         byte[] bytes = TypeConversion.shortToByteArray((short) value, ByteOrder.LITTLE_ENDIAN);
