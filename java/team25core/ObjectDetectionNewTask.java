939b75ca9eed41b21c6d6260fbc6a4e5c19a7757
Cindy Oda <odacindy@yahoo.com>
2024-01-27 00:23:23 -0800

    added MEDIUM_VERBOSITY and num -m "added MEDIUM_VERBOSITY and numCurrentDetections of AprilTags, added enum ErrDataType, which i think i will end up removing,ich i'll remove, since it is ugly, moved some of the drive error stuff from auto opmode to tterkel ObjectDetectionNewTask and added getDriveErrors method to get the drive error data and printErrorData, added setDesiredDistance, setDriveGains, and setMaxAutoance, setDriveGains, and setMaxAuto, setVerbosity"

--- java/team25core/ObjectDetectionNewTask.java
+++ java/team25core/ObjectDetectionNewTask.java
@@ -6,6 +6,7 @@ import android.util.Size;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
@@ -58,7 +59,9 @@ public class ObjectDetectionNewTask extends RobotTask {
     private Telemetry.Item aprilTagCenterYTlm;
 
     private int debug = 1;
-    private static int HIGH_VERBOSITY = 2;
+    private static int HIGH_VERBOSITY = 5;
+    private static int MEDIUM_VERBOSITY = 3;
+    private int numCurrentDetections = 0;
 
 
     // not sure if this is needed with husky lens
@@ -83,6 +86,20 @@ public class ObjectDetectionNewTask extends RobotTask {
         OBJECT2_DETECTED,
         APRILTAG_DETECTED,
         UNKNOWN_DETECTED,
+    }
+    public enum ErrDataType {
+        DRIVE(0),
+        TURN(1),
+        STRAFE(2),
+        RANGE_ERROR(3),
+        HEADING_ERROR(4),
+        YAW_ERROR(5);
+
+        private final int value;
+        ErrDataType(final int newValue) {
+            this.value = newValue;
+        }
+        public int getValue() {return value; }
     }
 
     public enum ObjectKind {
@@ -109,6 +126,83 @@ public class ObjectDetectionNewTask extends RobotTask {
     private boolean targetFound = false;
 
     private int desiredTagID;
+    private boolean moreTlm = false;
+
+    //  Set the GAIN constants to control the relationship between the measured position error,
+    //  and how much power is applied to the drive motors to correct the error.
+    //  Drive = Error * Gain    Make these values smaller for smoother control, or larger
+    //  for a more aggressive response.
+    private double speedGain = 0.02;   //  Forward Speed Control "Gain". eg: Ramp up to 50% power at a 25 inch error.   (0.50 / 25.0)
+    private double strafeGain = 0.015;   //  Strafe Speed Control "Gain".  eg: Ramp up to 25% power at a 25 degree Yaw error.   (0.25 / 25.0)
+    private double turnGain = 0.01;   //  Turn Control "Gain".  eg: Ramp up to 25% power at a 25 degree error. (0.25 / 25.0)
+
+    private double maxAutoSpeed = 0.5;   //  Clip the approach speed to this max value (adjust for your robot)
+    private double maxAutoStrafe = 0.5;   //  Clip the approach speed to this max value (adjust for your robot)
+    private double maxAutoTurn = 0.3;   //  Clip the turn speed to this max value (adjust for your robot)
+
+    private double desiredDistance;
+    double[] errorData = new double[6];
+
+    public double[] getDriveErrors(AprilTagDetection tag) {
+
+        double rangeError = (tag.ftcPose.range - desiredDistance);
+        double headingError = tag.ftcPose.bearing;
+        double yawError = tag.ftcPose.yaw;
+
+
+        errorData[0] = Range.clip(rangeError * speedGain, -maxAutoSpeed, maxAutoSpeed);
+        errorData[ErrDataType.TURN.getValue()] = Range.clip(headingError * turnGain, -maxAutoTurn, maxAutoTurn);
+        errorData[ErrDataType.STRAFE.getValue()] = Range.clip(-yawError * strafeGain, -maxAutoStrafe, maxAutoStrafe);
+        errorData[ErrDataType.RANGE_ERROR.getValue()] = (double) rangeError;
+        errorData[ErrDataType.HEADING_ERROR.getValue()] = (double) headingError;
+        errorData[ErrDataType.YAW_ERROR.getValue()] = (double) yawError;
+
+        printErrorData();
+        return(errorData);
+    }
+
+    public void printErrorData() {
+        myTelemetry.addData("Auto","Drive %5.2f, Strafe %5.2f, Turn %5.2f\n \r" +
+                        "rangeError=%5.2f, headingError=%5.2f, yawError=%5.2f ",
+                errorData[ErrDataType.DRIVE.getValue()],
+                errorData[ErrDataType.STRAFE.getValue()],
+                errorData[ErrDataType.TURN.getValue()],
+                errorData[ErrDataType.RANGE_ERROR.getValue()],
+                errorData[ErrDataType.HEADING_ERROR.getValue()],
+                errorData[ErrDataType.YAW_ERROR.getValue()]);
+        //myTelemetry.update();
+    }
+
+//    public class DriveErrors {
+//        private double drive = 0;
+//        private double strafe = 0;
+//        private double turn = 0;
+//
+//        public DriveErrors(AprilTagDetection tag) {
+//            double rangeError = (tag.ftcPose.range - desiredDistance);
+//            double headingError = tag.ftcPose.bearing;
+//            double yawError = tag.ftcPose.yaw;
+//
+//            this.drive = Range.clip(rangeError * speedGain, -maxAutoSpeed, maxAutoSpeed);
+//            this.turn = Range.clip(headingError * turnGain, -maxAutoTurn, maxAutoTurn);
+//            this.strafe = Range.clip(-yawError * strafeGain, -maxAutoStrafe, maxAutoStrafe);
+//        }
+//        public void dataUpdate(AprilTagDetection tag) {
+//            double rangeError = (tag.ftcPose.range - desiredDistance);
+//            double headingError = tag.ftcPose.bearing;
+//            double yawError = tag.ftcPose.yaw;
+//
+//            this.drive = Range.clip(rangeError * speedGain, -maxAutoSpeed, maxAutoSpeed);
+//            this.turn = Range.clip(headingError * turnGain, -maxAutoTurn, maxAutoTurn);
+//            this.strafe = Range.clip(-yawError * strafeGain, -maxAutoStrafe, maxAutoStrafe);
+//        }
+//        public void printDriveErrorTlm() {
+//            myTelemetry.addData("Auto", "Drive %5.2f, Strafe %5.2f, Turn %5.2f ", drive, strafe, turn);
+//
+//        }
+//    }
+
+
 
     public class ObjectDetectionEvent extends RobotEvent {
 
@@ -166,6 +260,19 @@ public class ObjectDetectionNewTask extends RobotTask {
         }
     }
 
+    public void setDesiredDistance( double myDesiredDistance ) {
+        desiredDistance = myDesiredDistance;
+    }
+    public void setDriveGains(double speed, double strafe, double turn) {
+        speedGain = speed;
+        strafeGain = strafe;
+        turnGain = turn;
+    }
+    public void setMaxAuto(double speed, double strafe, double turn) {
+        maxAutoSpeed = speed;
+        maxAutoStrafe = strafe;
+        maxAutoTurn = turn;
+    }
 
     public void initAprilTagTlm(Telemetry myTelemetry) {
         // add "key" information to telemetry
@@ -232,7 +339,9 @@ public class ObjectDetectionNewTask extends RobotTask {
         myAprilTagProcessorBuilder.setDrawCubeProjection(true); // Default: false
 
         //these are values we got after camera callibration using 3DFZephyr
-        myAprilTagProcessorBuilder.setLensIntrinsics(1431.77, 1431.77,949.511, 558.915);
+        // we needed to comment the following out, because it was hosing the
+        // XYZ, RBE because we have a logitech C920 and
+        //myAprilTagProcessorBuilder.setLensIntrinsics(1431.77, 1431.77,949.511, 558.915);
 
 
 
@@ -389,15 +498,26 @@ public class ObjectDetectionNewTask extends RobotTask {
         }
     }
 
-
+    public void setVerbosity(int verbosity) {
+        debug = verbosity;
+    }
 
     public void printAprilTagTlm(AprilTagDetection myDetection) {
         if (myDetection.metadata != null) {
-            myTelemetry.addLine(String.format("\n==== (ID %d) %s", myDetection.id, myDetection.metadata.name));
-            myTelemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", myDetection.ftcPose.x, myDetection.ftcPose.y, myDetection.ftcPose.z));
-            myTelemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", myDetection.ftcPose.pitch, myDetection.ftcPose.roll, myDetection.ftcPose.yaw));
-            myTelemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", myDetection.ftcPose.range, myDetection.ftcPose.bearing, myDetection.ftcPose.elevation));
+            if (moreTlm) {
+                myTelemetry.addLine(String.format("\n==== (ID %d) %s", myDetection.id, myDetection.metadata.name));
+                myTelemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", myDetection.ftcPose.x, myDetection.ftcPose.y, myDetection.ftcPose.z));
+                myTelemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", myDetection.ftcPose.pitch, myDetection.ftcPose.roll, myDetection.ftcPose.yaw));
+                myTelemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", myDetection.ftcPose.range, myDetection.ftcPose.bearing, myDetection.ftcPose.elevation));
+            } else {
+                myTelemetry.addData("Found", "ID %d (%s)", myDetection.id, myDetection.metadata.name);
+                myTelemetry.addData("Range",  "%5.1f inches", myDetection.ftcPose.range);
+                myTelemetry.addData("Bearing","%3.0f degrees", myDetection.ftcPose.bearing);
+                myTelemetry.addData("Yaw","%3.0f degrees", myDetection.ftcPose.yaw);
+                myTelemetry.addData("# April Tag Detections", "%d", numCurrentDetections);
 
+                printErrorData();
+            }
         } else {
             myTelemetry.addLine(String.format("\n==== (ID %d) Unknown", myDetection.id));
             myTelemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", myDetection.center.x, myDetection.center.y));
@@ -413,26 +533,27 @@ public class ObjectDetectionNewTask extends RobotTask {
     protected void processAprilTags() {
         List<AprilTagDetection> currentDetections = aprilTag.getDetections();
         //numAprilTagsDetectedTlm.setValue(currentDetections.size());
-        if (debug > HIGH_VERBOSITY) {
+        if (debug >= HIGH_VERBOSITY) {
             myTelemetry.addData("# AprilTags Detected", currentDetections.size());
         }
+        numCurrentDetections = currentDetections.size();
         for (AprilTagDetection detection : currentDetections) {
             // Look to see if we have size info on this tag.
             if (detection.metadata != null) {
-                if (debug > HIGH_VERBOSITY) {
+                if (debug >= MEDIUM_VERBOSITY) {
                     printAprilTagTlm(detection);
                 }
                 //  Check to see if we want to track towards this tag.
                 if ((desiredTagID < 0) || (detection.id == desiredTagID)) {
                     // Yes, we want to use this tag.
                     targetFound = true;
-                    if (debug > HIGH_VERBOSITY) {
+                    if (debug >= HIGH_VERBOSITY) {
                         myTelemetry.addData("Desired tag detected: ", "true");
                     }
                     desiredTag = detection;
                     break;  // don't look any further.
                 } else {
-                    if (debug > HIGH_VERBOSITY) {
+                    if (debug >= HIGH_VERBOSITY) {
                         // This tag is in the library, but we do not want to track it right now.
                         myTelemetry.addData("Skipping", "Tag ID %d is not desired", detection.id);
                         myTelemetry.addData("Desired tag detects: ", "false");
@@ -445,7 +566,6 @@ public class ObjectDetectionNewTask extends RobotTask {
         }   // end for() loop
 
         if (targetFound) {
-            printAprilTagTlm(desiredTag);
             robot.queueEvent(new ObjectDetectionNewTask.TagDetectionEvent(this, EventKind.APRIL_TAG_DETECTED, desiredTag));
         }
 
@@ -475,14 +595,14 @@ public class ObjectDetectionNewTask extends RobotTask {
 
         // Make sure camera is streaming before we try to set the exposure controls
         if (myVisionPortal.getCameraState() != VisionPortal.CameraState.STREAMING) {
-            if (debug > HIGH_VERBOSITY) {
+            if (debug >= HIGH_VERBOSITY) {
                 myTelemetry.addData("Camera", "Waiting");
                 myTelemetry.update();
             }
             return;
         }
 
-        if (debug > HIGH_VERBOSITY) {
+        if (debug >= HIGH_VERBOSITY) {
             myTelemetry.addData("Camera", "Ready");
             myTelemetry.update();
         }
@@ -491,14 +611,22 @@ public class ObjectDetectionNewTask extends RobotTask {
         ExposureControl exposureControl = myVisionPortal.getCameraControl(ExposureControl.class);
         if (exposureControl.getMode() != ExposureControl.Mode.Manual) {
             exposureControl.setMode(ExposureControl.Mode.Manual);
-            //sleep(50);
+            mysleep(50);
         }
         exposureControl.setExposure((long)myExposureMS, TimeUnit.MILLISECONDS);
-        //sleep(20);
+        mysleep(20);
         GainControl gainControl = myVisionPortal.getCameraControl(GainControl.class);
         gainControl.setGain(myGain);
-        //sleep(20);
+        mysleep(20);
     }
+    public final void mysleep(long milliseconds) {
+        try {
+            Thread.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
 
     public void resumeStreaming() {
         myVisionPortal.resumeStreaming();
